e35d7a150b4e3e0a985e1b7fada90bcc724b3962
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-03-22 07:02:39 -0300

    Include Gugu in seed as admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 # /db/seeds.rb
 
-User.create name:'Gabriel Braico Dornas', admin:true, email: 'gabrielbdornas@gmail.com', password: 123456, password_confirmation:123456
+User.delete_all
+User.create name:'Gabriel Braico Dornas', admin:true, email:'gabrielbdornas@gmail.com', password:123456, password_confirmation:123456
+User.create name:'Antônio Augusto Braico', admin:true, email:'antonioaugustobraighi@gmail.com', password:123456, password_confirmation:123456
 
